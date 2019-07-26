a02d3e3f1b49abf87b614d3033ed544fefaa6b61
Bonifase <orwabonifase@gmail.com>
2019-07-26 13:22:34 +0300

    test

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const Player = require("../models/player");
 
 router.get("/", (req, res) => {
   res.send("hello");
 });
 router.post("/add", (req, res) => {
-  res.send("Posted");
+  let newPlayer = new Player(req.body);
+  newPlayer
+    .save()
+    .then(item => {
+      res.send(`${item.fname} saved to database`);
+    })
+    .catch(err => {
+      res.status(400).send("unable to save to database");
+    });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -6,6 +6,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/data", routes);
+app.use("/player", routes);
 
 app.listen(4000, () => console.log(`server running on port ${4000}`));
